55feef2ff614fc69c74fb68a57bde3b719e7bd5d
seabassdk <s.blichfeldt@live.com>
2020-09-11 11:55:08 +0200

    restroom utils installed

---
+++ build/precache-manifest.8bcf9a82221991f982c67aa2e59575da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d0ebe72568f8df1e4c4843c59ee8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4933cff0b5d3641eada6",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b862196aeaf65854430d",
+    "url": "/static/css/main.074fd6c6.chunk.css"
+  },
+  {
+    "revision": "4933cff0b5d3641eada6",
+    "url": "/static/js/2.2850a4f3.chunk.js"
+  },
+  {
+    "revision": "d0bd5520e65e7a3ac323a2a37adf4aab",
+    "url": "/static/js/2.2850a4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b862196aeaf65854430d",
+    "url": "/static/js/main.b708aeb3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e5935b997241b7e1e308c1c56874dce3",
+    "url": "/static/media/cafudda.e5935b99.jpg"
+  },
+  {
+    "revision": "c236e5b6c8474ea083fa4a529848e5bb",
+    "url": "/static/media/logo.c236e5b6.png"
+  },
+  {
+    "revision": "bc8f3e2a403c48d5d7ab334d9578dd33",
+    "url": "/static/media/zlogo.bc8f3e2a.png"
+  }
+]);
