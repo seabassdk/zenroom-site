68c6a72a968b6ff44015b1fd6fdbd14ebb164d6a
andrea-dintino <andrea@tabbles.net>
2020-09-24 10:58:17 +0200

    latest syntax highlighter

---
+++ build/build/precache-manifest.33dc6231c4b21465b50a5ca63a600aa2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f785078fedc4cad04e253c748a7908a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea9fd9d38fddf4f9678",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "50708ba35c6a9b2ef34e",
+    "url": "/static/css/main.21e938e6.chunk.css"
+  },
+  {
+    "revision": "2ea9fd9d38fddf4f9678",
+    "url": "/static/js/2.96855f58.chunk.js"
+  },
+  {
+    "revision": "d0bd5520e65e7a3ac323a2a37adf4aab",
+    "url": "/static/js/2.96855f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50708ba35c6a9b2ef34e",
+    "url": "/static/js/main.af7b5db9.chunk.js"
+  },
+  {
+    "revision": "aaf9d8f0ccdf4ed3ac77",
+    "url": "/static/js/runtime-main.10daf916.js"
+  },
+  {
+    "revision": "e5935b997241b7e1e308c1c56874dce3",
+    "url": "/static/media/cafudda.e5935b99.jpg"
+  },
+  {
+    "revision": "c236e5b6c8474ea083fa4a529848e5bb",
+    "url": "/static/media/logo.c236e5b6.png"
+  },
+  {
+    "revision": "bc8f3e2a403c48d5d7ab334d9578dd33",
+    "url": "/static/media/zlogo.bc8f3e2a.png"
+  }
+]);
